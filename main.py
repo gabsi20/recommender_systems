2857e2f702d3f48fbb0025be9a95d4c869f85596
Daniel Trojer <dtrojer.mmt-b2014@fh-salzburg.ac.at>
2018-01-09 13:37:27 +0100

    refactor: extract file module

--- main.py
+++ main.py
@@ -1,26 +1,15 @@
-import csv
 import numpy as np
 import random
-
-
-def read_from_file(filename):
-    data = []
-    with open(filename, 'r') as f:                 
-        reader = csv.reader(f, delimiter='\t')     
-        headers = reader.next()                    
-        for row in reader:
-            item = row
-            data.append(item)
-    return data
+import file
 
 UAM_FILE = "data/C1ku_UAM.txt"                    
 ARTISTS_FILE = "data/C1ku_idx_artists.txt"       
 USERS_FILE = "data/C1ku_idx_users.txt"   
 ARTISTS_EXTENDED = "data/C1ku_artists_extended.csv"  
-ARTISTS = read_from_file(ARTISTS_FILE)
-USERS = read_from_file(USERS_FILE)
+ARTISTS = file.read_from_file(ARTISTS_FILE)
+USERS = file.read_from_file(USERS_FILE)
 UAM = np.loadtxt(UAM_FILE, delimiter='\t', dtype=np.float32)
-ARTISTS_DATA = read_from_file(ARTISTS_EXTENDED)
+ARTISTS_DATA = file.read_from_file(ARTISTS_EXTENDED)
 
 def random_artist_recommender(user):
     recommendation_pool = []
@@ -46,5 +35,4 @@ def random_user_recommender(user):
     return random.sample(recommendation_pool,10)
 
 print random_user_recommender(100)
-
 print random_artist_recommender(100)
