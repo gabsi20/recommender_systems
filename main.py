409f4b41acd93248aa43a23461e5be3c3958c872
Daniel Trojer <dtrojer.mmt-b2014@fh-salzburg.ac.at>
2018-01-09 16:52:37 +0100

    feat: add popularity recommender and refactor random artist

--- main.py
+++ main.py
@@ -11,13 +11,12 @@ USERS = file.read_from_file(USERS_FILE)
 UAM = np.loadtxt(UAM_FILE, delimiter='\t', dtype=np.float32)
 ARTISTS_DATA = file.read_from_file(ARTISTS_EXTENDED)
 
+MAX_RECOMMENDATIONS = 10
+
 def random_artist_recommender(user):
-    recommendation_pool = []
-    user_playcounts =  UAM[user,:]
-    for idx, count in enumerate(user_playcounts):
-        if count == 0:
-            recommendation_pool.append(ARTISTS_DATA[idx][1])
-    return random.sample(recommendation_pool, 10)
+    user_playcounts = UAM[user, :]
+    recommendation_pool = np.where(user_playcounts == 0)[0]
+    return random.sample(recommendation_pool, MAX_RECOMMENDATIONS)
 
 def random_user_recommender(user):
     artist_pool = []
@@ -27,12 +26,19 @@ def random_user_recommender(user):
         for idx, count in enumerate(user_playcounts):
             if count > 0:
                 artist_pool.append(idx)
+
     recommendation_pool = []
     user_playcounts =  UAM[user,:]
     for idx, artist in enumerate(artist_pool):
         if user_playcounts[artist] == 0:
-            recommendation_pool.append(ARTISTS_DATA[artist][1])
+            recommendation_pool.append(artist)
     return random.sample(recommendation_pool,10)
+
+def popularity_recommender():
+    sums = np.sum(UAM, axis=0)
+    top_ranked_indizes = np.argsort(sums)[-MAX_RECOMMENDATIONS:]
+    return np.flip(top_ranked_indizes, axis=0)
 
 print random_user_recommender(100)
 print random_artist_recommender(100)
+print popularity_recommender()
