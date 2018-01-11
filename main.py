e1835a29f7e3107e2b114015a8dd440b44a4f460
Daniel Trojer <dtrojer.mmt-b2014@fh-salzburg.ac.at>
2018-01-11 14:38:59 +0100

    feat: fix evaluation

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import numpy as np
 import random
 import file
 import train
-import fetch
 from sets import Set
 from sklearn import cross_validation  
 import scipy.spatial.distance as scidist 
@@ -18,7 +17,6 @@ USERS = file.read_from_file(USERS_FILE)
 UAM = np.loadtxt(UAM_FILE, delimiter='\t', dtype=np.float32)
 ARTISTS_DATA = file.read_from_file(ARTISTS_EXTENDED)
 AAM = file.read_from_file(AAM_FILE)
-
 
 FOLDS = 10
 
@@ -40,7 +38,7 @@ def random_user_recommender(UAM, user, K):
 def popularity_recommender(UAM, _user, _K):
     sums = np.sum(UAM, axis=0)
     top_ranked_indizes = np.argsort(sums)
-    return top_ranked_indizes
+    return top_ranked_indizes[::-1]
 
 
 def get_nonzero_artists_from_users(users_playcounts):
@@ -51,16 +49,16 @@ def get_nonzero_artists_from_users(users_playcounts):
 
 
 def collaborative_filtering_recommender(UAM, user, K):
-    pc_vec = UAM[user,:]
+    pc_vec = UAM[user, :]
     sim_users = np.zeros(shape=(UAM.shape[0]), dtype=np.float32)
-    for u in range(0, UAM.shape[0]):
-        sim_users[u] = 1.0 - scidist.cosine(pc_vec, UAM[u,:])   
-    sort_idx = np.argsort(sim_users)      
+    for user_index in range(0, UAM.shape[0]):
+        sim_users[user_index] = 1.0 - scidist.cosine(pc_vec, UAM[user_index,:])   
+    sort_idx = np.argsort(sim_users)
     neighbours_idx = sort_idx[-1-K:-1]
     counter = {}
     for idx, neighbour in enumerate(neighbours_idx):
-        artist_idx_u = np.nonzero(UAM[user,:])                 
-        artist_idx_n = np.nonzero(UAM[neighbour,:])      
+        artist_idx_u = np.nonzero(UAM[user, :])
+        artist_idx_n = np.nonzero(UAM[neighbour, :])
         recommended_artists_idx = np.setdiff1d(artist_idx_n[0], artist_idx_u[0])
         for artist_idx in recommended_artists_idx:
             if artist_idx in counter:
@@ -70,55 +68,60 @@ def collaborative_filtering_recommender(UAM, user, K):
     return sorted(counter, key=counter.get, reverse=True)
 
 
-def evaluate(method):
+def evaluate(method, color="r"):
+    recommendation_count = 10
+    MAX_USERS = 5
+
+    sample_users = random.sample(range(0, UAM.shape[0]), MAX_USERS)
+
     precisions = []
     recalls = []
 
-    MAX_RECOMMENDATIONS = 10
-    MAX_USERS = 10
+    for recommendations_count in [1,2,5,10,20,50,100]:
 
-    for user_index in range(0, MAX_USERS):
         avg_precision = 0
         avg_recall = 0
 
-        user = UAM[user_index:]
+        for user_index in sample_users:
+            user = UAM[user_index:]
 
-        folds = cross_validation.KFold(len(user), n_folds=FOLDS)
+            folds = cross_validation.KFold(len(user), n_folds=FOLDS)
 
-        for _train_artists, test_artists in folds:
-            training_uam = UAM.copy()
-            training_uam[user_index, test_artists] = 0.0
+            for _train_artists, test_artists in folds:
+                training_uam = UAM.copy()
+                training_uam[user_index, test_artists] = 0.0
 
-            result_indizes = method(training_uam, user_index, 100)[0:MAX_RECOMMENDATIONS-1]
-            correct_indizes = np.intersect1d(user[test_artists], result_indizes)
+                result_indizes = method(training_uam, user_index, 100)[0:recommendations_count]
+                correct_indizes = np.intersect1d(user[test_artists], result_indizes)
 
-            true_positives = len(correct_indizes)
+                true_positives = len(correct_indizes)
 
-            precision = 100.0 if len(result_indizes) == 0 else 100.0 * true_positives / len(result_indizes)
-            recall = 100.0 if len(test_artists) == 0 else 100.0 * true_positives / len(test_artists)
+                precision = 100.0 if len(result_indizes) == 0 else 100.0 * true_positives / len(result_indizes)
+                recall = 100.0 if len(test_artists) == 0 else 100.0 * true_positives / len(test_artists)
 
-            avg_precision += precision / (FOLDS)
-            avg_recall += recall / (FOLDS)
-            
+                avg_precision += precision
+                avg_recall += recall
+
         f_measure = 2 * ((avg_precision * avg_recall) / (precision + recall)) if (precision + recall) else 0.00
-        
-        precisions.append(avg_precision / MAX_USERS)
-        recalls.append(avg_recall / MAX_USERS)
 
-        print ("\n\nMean Average Precision: %.2f\nMean Average Recall %.2f" % (avg_precision, avg_recall))
-        print ("\nF-Measure: %.2f" % f_measure)
+        precisions.append(avg_precision / (FOLDS * MAX_USERS) )
+        recalls.append(avg_recall / (FOLDS * MAX_USERS))
 
-    sort_indizes = np.argsort(precisions)
+        print ("\n\nMean Average Precision: %.2f\nMean Average Recall %.2f" % (avg_precision / (FOLDS * MAX_USERS), avg_recall / (FOLDS * MAX_USERS)))
 
-    plt.plot(recalls, precisions, 'ro')
-    plt.axis([0, max(recalls), 0, max(precisions)])
-    plt.show()
+    plt.plot(recalls, precisions, color)
     plt.savefig('./results/' + method.__name__ + '.png')
 
-evaluate(random_artist_recommender)
-evaluate(random_user_recommender)
-evaluate(popularity_recommender)
-evaluate(collaborative_filtering_recommender)
+evaluate(random_artist_recommender, 'r')
+evaluate(random_user_recommender, 'g')
+evaluate(popularity_recommender, 'b')
+evaluate(collaborative_filtering_recommender, 'y')
+
+plt.axis()
+plt.show()
+plt.savefig('./results/compared.png')
+
+print "Done."
 
 def content_based_recommender(user, K):
     return random_user_recommender(100)
